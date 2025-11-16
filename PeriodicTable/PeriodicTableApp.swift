52f66e4e2c8df205d1ba654a2772adee477fdef1
Nina Maeva Mazadiego Cortes <mazcortmae@gmail.com>
2025-11-15 21:44:04 -0600

    Initial Commit

---
+++ PeriodicTable/PeriodicTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeriodicTableApp.swift
+//  PeriodicTable
+//
+//  Created by Maeva Mazadiego on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PeriodicTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PeriodicTableTests/PeriodicTableTests.swift
@@ -0,0 +1,17 @@
+//
+//  PeriodicTableTests.swift
+//  PeriodicTableTests
+//
+//  Created by Maeva Mazadiego on 15/11/25.
+//
+
+import Testing
+@testable import PeriodicTable
+
+struct PeriodicTableTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
